4e24ad8ec666cb15a8ffaebe35725e8c9019aea8
Rishika Agarwal <rishika.agarwal@rupeepower.com>
2019-05-15 14:58:10 +0530

    first commit

---
+++ app/src/main/java/com/example/baseproject/activity/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.baseproject.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.baseproject.R
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+}
